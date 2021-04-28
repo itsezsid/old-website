2def0449c7fd96a773b57b784ef201c95f6c92a3
Siddharth Iyer <siddharthiyer@outlook.in>
2021-04-28 15:07:43 +0000

    Initial commit

---
+++ content/featured/MagicID/index.md
@@ -0,0 +1,13 @@
+---
+date: '2'
+title: 'MagicID'
+cover: '../../projects/images/magicid.png'
+tech:
+  - Crystal
+  - GraphQL
+  - Oauth 2
+  - JWT Tokens
+showInProjects: true
+---
+
+An authentication server which runs on react and crystal lang. The frontend uses GraphQL to communicate with the backend server which returns a JWT token on successful authentication. The backend is written from scratch using the Crystal.
---
+++ src/components/sections/about.js
@@ -0,0 +1,197 @@
+import React, { useEffect, useRef } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import { srConfig } from "@config";
+import sr from "@utils/sr";
+import { usePrefersReducedMotion } from "@hooks";
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: "▹";
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--lightest-slate);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      avatar: file(
+        sourceInstanceName: { eq: "images" }
+        relativePath: { eq: "me.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 500, traceSVG: { color: "#ececec" }) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `);
+
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = [
+    "JavaScript",
+    "Tensorflow",
+    "Crystal",
+    "GraphQL",
+    "Node.js",
+    "Python"
+  ];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hi, I'm Siddharth. I’m a high school senior student at Delhi
+              Public School Bangalore South currently working as a Chief
+              Engineering Officer at Foretag Inc. I contribute to
+              back-end development at Foretag using Crystal, GraphQL,
+              Javascript, etc. 
+            </p>
+            <p>
+              
+              I want to integrate technology with our health
+              care system with the help of drones. I love computers, physics,
+              electronics and I want to be an innovator to make this world a
+              better place.
+            </p>
+
+            <p>Here are a few technologies I've been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <Img
+              fluid={data.avatar.childImageSharp.fluid}
+              alt="Avatar"
+              className="img"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
