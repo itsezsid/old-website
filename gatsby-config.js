f52c087ee2a313cfb0733b91bf7a8174d6de659d
Siddharth Iyer <itsezsid@outlook.in>
2021-04-29 23:46:06 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "itsezsid",
-        short_name: "sid",
+        name: `itsezsid`,
+        short_name: `sid`,
         start_url: "/",
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: "minimal-ui",
-        icon: "src/images/logo.png"
+        icon: `src/images/logo.png`
       }
     },
     `gatsby-plugin-offline`,
