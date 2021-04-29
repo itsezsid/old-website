a71d04ac0defd235a1cea19346bf939b28a374e4
Siddharth Iyer <itsezsid@outlook.in>
2021-04-29 09:16:57 +0530

    Favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: "minimal-ui",
-        icon: "/logo.png"
+        icon: "src/images/favicon/favicon.ico"
       }
     },
     `gatsby-plugin-offline`,
